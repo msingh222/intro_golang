433767f453ef1a7d91d720b41b89735488c65d90
msingh222 <msingh22@ncsu.edu>
2018-06-14 22:21:44 -0400

    fuctions

--- finction.go
+++ finction.go
@@ -2,11 +2,19 @@ package main
 
 import "fmt"
 
-func addition(x int , y int)  int {
+func addition(x int , y int) int {
 	return x + y
 
 }
 
+func swap(a,b string)  (string,string) {
+	return b, a
+
+}
 func main ()  {
 	fmt.Println("7 + 10  =  ", addition(7,10))
+	a, b := swap("hello","world")
+
+	fmt.Println(a,b)
+
 }
